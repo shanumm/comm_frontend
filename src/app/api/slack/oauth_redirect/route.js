b5b09e047cd7ddfeb9564a0ab3f1b6bdeff85ba5
shanumm <shanumishra199@gmail.com>
2024-07-21 15:16:42 +0530

    udpate

--- src/app/api/slack/oauth_redirect/route.js
+++ src/app/api/slack/oauth_redirect/route.js
@@ -2,13 +2,13 @@ import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 
 export async function GET(req, res) {
-  console.log(res, "res");
-  console.log(req, "req");
+  if (res) console.log(res, "res");
+  if (req) console.log(req, "req");
   // console.log(JSON.parse(req), " json req");
   console.log(headers);
   return NextResponse.json(
-    { res: res },
-    { req: req },
+    { res: res ? res : "no res" },
+    { req: req ? req : "no req" },
     {
       status: 200,
     }
